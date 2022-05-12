7923977a71e5d6eed005a5d046809c0a4b779515
wghaen2681 <wghaen2681@gmail.com>
2022-05-12 16:09:49 +0800

    Step 2: Set up the TranslateModule and TranslateService

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import {TranslateService} from "@ngx-translate/core";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
+
 export class AppComponent {
   title = 'angular_translateservice';
+  constructor (private translate: TranslateService) {
+    translate.setDefaultLang('en');    
+    translate.use('en');  
+  }
 }
